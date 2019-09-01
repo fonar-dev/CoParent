345611136ed6927c02a9f0a1df376ed06e072c3a
Fonar <rick.hambrick@solverant.com>
2019-09-01 10:44:41 -0700

    Initial Commit

---
+++ CoParent/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CoParent
+//
+//  Created by Rick Hambrick on 9/1/19.
+//  Copyright © 2019 Rick Hambrick. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoParent/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CoParent
+//
+//  Created by Rick Hambrick on 9/1/19.
+//  Copyright © 2019 Rick Hambrick. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
